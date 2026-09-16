2369a8091c180dd47146a6b374f9c64ea343f48c
LE GOFF Francois OBS/OBF <QVNQ4043@orange.com>
2026-09-16 16:59:05 +0200

    adding table describing books

--- app/BookSection.tsx
+++ app/BookSection.tsx
@@ -1,16 +1,24 @@
-export default function BookSection({ bookCount }: {bookCount: number}) {
-    return (
-        <main>
-            <section>
-                <h2>
-                    Mes livres
-                </h2>
-                <p>Découvre, organise et garde une trace de tes lectures</p>
-                <label htmlFor="site-search">Rechercher sur le site :</label>
-                <input type="search" id="site-search" name="q" />
-                <button>Rechercher</button>
-                <p>Tu as actuellement {bookCount} livres</p>
-            </section>
-        </main>
-    )
+export default function BookSection({
+  books,
+}: {
+  books: any[];
+}) {
+  return (
+    <section>
+      <h2>Mes livres</h2>
+
+      <p>Découvre, organise et garde une trace de tes lectures</p>
+
+      <label htmlFor="site-search">Rechercher sur le site :</label>
+      <input type="search" id="site-search" name="q" />
+      <button>Rechercher</button>
+
+      {books.map((book) => (
+        <div key={book.id}>
+          <h3>{book.title}</h3>
+          <p>{book.author}</p>
+        </div>
+      ))}
+    </section>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,29 @@
+import { supabase } from "../lib/supabase";
 import BookSection from "./BookSection";
 
-export default function Home() {
+export default async function Home() {
+
+  //Récupération du nombre de livres présents en base
+  const {count } = await supabase
+    .from("books")
+    .select("*", { count: "exact" });
+
+  const bookCount = count ?? 0;
+
+  //Récupération du détail des livres
+  const { data: books } = await supabase
+  .from("books")
+  .select("*")
+  .order("created_at", { ascending: false });
+
+  console.log(books);
+
   return (
     <main>
       <h1>Book Collector</h1>
       <p>Ma bibliothèque de livres.</p>
 
-      <BookSection bookCount={42} />
+      <BookSection books={books} />
     </main>
   );
 }
